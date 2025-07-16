d1c3b94e836a00ff2628225279beaec4eac46876
LuckyColleges <collegelucky37@gmail.com>
2025-07-16 21:17:46 +0700

    commit lucky 1

--- backend/AddUser.js
+++ backend/AddUser.js
@@ -3,11 +3,11 @@ const db = require('./config/db'); // pastikan path benar
 
 async function insertUser() {
     try {
-        const username = 'resep01';
-        const passwordPlain = 'resep1';
-        const full_name = 'Resepsionis A';
-        const email = 'resep@hotel.com';
-        const role = 'receptionist'; // Sesuai ENUM: admin, receptionist, manager, staff
+        const username = 'admin01';
+        const passwordPlain = 'admin1';
+        const full_name = 'Admin Utama';
+        const email = 'admin@hotel.com';
+        const role = 'admin'; // Sesuai ENUM: admin, receptionist, manager, staff
 
         const hashedPassword = await bcrypt.hash(passwordPlain, 10);
 
